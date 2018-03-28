9a724deed3a98dff4c5f27bcd0c12f529e2e55b9
Dan Milon <i@danmilon.me>
2018-03-28 15:26:59 +0300

    move to opencsa

--- root/srv/www/wp-config.php
+++ root/srv/www/wp-config.php
@@ -62,7 +62,7 @@ define('NONCE_SALT',       'put your unique phrase here');
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'csa_';
+$table_prefix  = 'ocsa_';
 
 /**
  * WordPress Localized Language, defaults to English.
